ccb121eef2ff169dd74dfbb06fde4bc0c2151376
deanvlue <munoz.josecarlos@gmail.com>
2015-10-11 23:02:47 -0500

    Agrega busqueda y sorting

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -2,18 +2,52 @@ angular
   .module('socially')
   .controller('PartiesListCtrl', ['$scope','$meteor', function ($scope, $meteor){
 
-    $scope.parties = $meteor.collection(Parties);
+    $scope.page = 1;
+    $scope.perPage = 3;
+    $scope.sort = {name: 1};
+    $scope.orderProperty = '1';
+
+    //$scope.parties = $meteor.collection(Parties);
+    //$scope.parties = $meteor.collection(Parties).subscribe("parties");
+
+    $scope.parties = $meteor.collection(function(){
+      return Parties.find({},{
+        sort: $scope.getReactively('sort')
+      });
+    });
+
+    $meteor.autorun($scope, function(){
+      //$scope.parties = $meteor.collection('parties');
+      $meteor.subscribe("parties",{
+        limit: parseInt($scope.getReactively('perPage')),
+        skip: parseInt($scope.getReactively('page') -1) * parseInt($scope.getReactively('perPage')),
+        sort: $scope.getReactively('sort')
+     }, $scope.getReactively('search')).then(function() {
+        $scope.partiesCount = $meteor.object(Counts ,'numberOfParties', false);
+      });
+    });
+
 
     $scope.remove = function(party){
       if (confirm("En realidad desea borrar: " + party.name + "?")){
         $scope.parties.remove(party);
       }
-    }
+    };
 
     $scope.removeAll = function(){
       if (confirm("En realidad desea borrar todas las fiestas?")){
         $scope.parties.remove();
       }
-    }
+    };
+
+    $scope.pageChanged = function(newPage) {
+     $scope.page = newPage;
+   };
+
+    $scope.$watch('orderProperty', function(){
+      if($scope.orderProperty)
+        $scope.sort = {name: parseInt($scope.orderProperty)};
+    });
+
 
   }]);
--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,7 +1,10 @@
 angular
   .module("socially")
   .controller('PartyDetailsCtrl',['$scope','$stateParams', '$meteor', function ($scope, $stateParams, $meteor){
-    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+    //$scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+    $scope.$meteorSubscribe('parties');
 
     $scope.save = function(){
       $scope.party.save();
